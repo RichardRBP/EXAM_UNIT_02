8be2e3ce6544eb9559166fb8aba8e44bcf068751
Richard Braul <richardzz@hotmail.com>
2022-07-30 19:06:06 -0500

    First Commit Cracks, Mastodontes, Bufalos, Bestias, Dioses

---
+++ src/main/java/com/ingenieriaweb/springboot/app/ExamUnit02Application.java
@@ -0,0 +1,13 @@
+package com.ingenieriaweb.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamUnit02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamUnit02Application.class, args);
+	}
+
+}
